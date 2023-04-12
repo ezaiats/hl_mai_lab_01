7752758688f046c0b99e8c33fd1560c29b526c96
Ekaterina <ee.zayats@gmail.com>
2023-04-12 21:24:51 +0300

    Changed ports

--- delivery_server/http_web_server.h
+++ delivery_server/http_web_server.h
@@ -68,7 +68,7 @@ protected:
         if (!_helpRequested)
         {
             database::Delivery::init();
-            ServerSocket svs(Poco::Net::SocketAddress("0.0.0.0", 8083));
+            ServerSocket svs(Poco::Net::SocketAddress("0.0.0.0", 8082));
             HTTPServer srv(new HTTPRequestFactory(DateTimeFormat::SORTABLE_FORMAT), svs, new HTTPServerParams);
             srv.start();
             waitForTerminationRequest();
